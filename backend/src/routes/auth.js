7c0443a107c8e81ae53823cc12975408ea196164
Muhammad Zain Ul Abideen <mzainulabideen918@gmail.com>
2026-06-10 22:21:02 +0500

    Delete backend directory

--- backend/src/routes/auth.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-const auth = require('../middleware/auth');
-const { login, me, changePassword } = require('../controllers/authController');
-router.post('/login', login);
-router.get('/me', auth, me);
-router.put('/password', auth, changePassword);
-module.exports = router;
